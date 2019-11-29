09f06321f0e53d5203e34b5d5b5bfd10012a06c9
Simon Li <orpheus+devel@gmail.com>
2019-11-29 23:42:46 +0000

    0.0.6

--- jupyter_offlinenotebook/__init__.py
+++ jupyter_offlinenotebook/__init__.py
@@ -3,8 +3,24 @@ from notebook.utils import url_path_join
 from notebook.base.handlers import IPythonHandler
 import os
 from tornado import web
-from traitlets import Unicode
+from traitlets import TraitType
 from traitlets.config import Configurable
+
+
+class Callable(TraitType):
+    """
+    A trait which is callable.
+    Classes are callable, as are instances
+    with a __call__() method.
+    """
+
+    info_text = 'a callable'
+
+    def validate(self, obj, value):
+        if callable(value):
+            return value
+        else:
+            self.error(obj, value)
 
 
 class OfflineNotebookHandler(IPythonHandler):
@@ -18,37 +34,48 @@ class OfflineNotebookHandler(IPythonHandler):
         to work if the user subsequently goes offline
         """
         config = self.settings['offline_notebook_config']
-        repoid = os.getenv(config.repoid_variable)
-        self.log.debug('OfflineNotebook repoid: %s=%s',
-                       config.repoid_variable, repoid)
-
-        binder_ref_url = os.getenv('BINDER_REF_URL')
-
-        binder_launch = os.environ.get('BINDER_LAUNCH_HOST')
-        binder_persistent_request = os.environ.get('BINDER_PERSISTENT_REQUEST')
-        if binder_launch and binder_persistent_request:
-            binder_persistent_url = binder_launch + binder_persistent_request
-        else:
-            binder_persistent_url = None
-
-        self.set_header('Content-Type', 'application/json')
-        self.write(json.dumps({
+        repoid = config.repository_id()
+        binder_ref_url = config.repository_ref_url()
+        binder_persistent_url = config.binder_persistent_url()
+        jcfg = json.dumps({
             'repoid': repoid,
             'binder_ref_url': binder_ref_url,
             'binder_persistent_url': binder_persistent_url,
-        }))
+        })
+        self.log.debug('OfflineNotebook config:%s ', jcfg)
+        self.set_header('Content-Type', 'application/json')
+        self.write(jcfg)
 
 
 class OfflineNotebookConfig(Configurable):
     """
     Holds server-side configuration
     """
-    repoid_variable = Unicode(
-        default_value='BINDER_REPO_URL',
+
+    repository_id = Callable(
+        default_value=lambda: os.getenv('BINDER_REPO_URL', ''),
         help="""
-        Name of the environment variable containing the repository identifier.
+        A callable that returns the repository ID.
         This is used when storing and retrieving notebooks.
-        E.g. BINDER_REPO_URL BINDER_REF_URL
+        Default is the value of the `BINDER_REPO_URL` environment variable.
+        """
+    ).tag(config=True)
+
+    repository_ref_url = Callable(
+        default_value=lambda: os.getenv('BINDER_REF_URL', ''),
+        help="""
+        A callable that returns the persistent Binder URL.
+        Default is the value of the `BINDER_REF_URL` environment variable.
+        """
+    ).tag(config=True)
+
+    binder_persistent_url = Callable(
+        default_value=lambda: (os.getenv('BINDER_LAUNCH_HOST', '') +
+                               os.getenv('BINDER_PERSISTENT_REQUEST', '')),
+        help="""
+        A callable that returns the repository reference URL.
+        Default is the values of the `BINDER_LAUNCH_HOST` and
+        `BINDER_PERSISTENT_REQUEST` environment variables.
         """
     ).tag(config=True)
 
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='jupyter-offlinenotebook',
-    version='0.0.5',
+    version='0.0.6',
     author='Simon Li',
     author_email='spli@dundee.ac.uk',
     packages=[
