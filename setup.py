8313167935c43942c53d8ca9b4df38255dda67a0
Simon Li <orpheus+devel@gmail.com>
2019-12-08 14:06:35 +0000

    0.0.9

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from glob import glob
 
 setup(
     name='jupyter-offlinenotebook',
-    version='0.0.8',
+    version='0.0.9',
     author='Simon Li',
     author_email='spli@dundee.ac.uk',
     packages=[
