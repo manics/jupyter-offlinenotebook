f56c38ab47d2424136be94747bd53b549b7afb4e
Simon Li <orpheus+devel@gmail.com>
2020-09-05 17:57:04 +0100

    Add black to pre-commit

--- jupyter_offlinenotebook/__init__.py
+++ jupyter_offlinenotebook/__init__.py
@@ -14,7 +14,7 @@ class Callable(TraitType):
     with a __call__() method.
     """
 
-    info_text = 'a callable'
+    info_text = "a callable"
 
     def validate(self, obj, value):
         if callable(value):
@@ -24,7 +24,6 @@ class Callable(TraitType):
 
 
 class OfflineNotebookHandler(IPythonHandler):
-
     @web.authenticated
     async def get(self):
         """
@@ -33,27 +32,29 @@ class OfflineNotebookHandler(IPythonHandler):
         This should be called once at the start, since the extension is meant
         to work if the user subsequently goes offline
         """
-        config = self.settings['offline_notebook_config']
-        jcfg = json.dumps({
-            'repoid': config.repository_id(),
-            'binder_repo_label': config.repository_label(),
-            'binder_ref_url': config.repository_ref_url(),
-            'binder_persistent_url': config.binder_persistent_url(),
-        })
-        self.log.debug('OfflineNotebook config:%s ', jcfg)
-        self.set_header('Content-Type', 'application/json')
+        config = self.settings["offline_notebook_config"]
+        jcfg = json.dumps(
+            {
+                "repoid": config.repository_id(),
+                "binder_repo_label": config.repository_label(),
+                "binder_ref_url": config.repository_ref_url(),
+                "binder_persistent_url": config.binder_persistent_url(),
+            }
+        )
+        self.log.debug("OfflineNotebook config:%s ", jcfg)
+        self.set_header("Content-Type", "application/json")
         self.write(jcfg)
 
 
 def _repo_label_from_binder_request():
     try:
-        repotype = os.getenv('BINDER_PERSISTENT_REQUEST', '').split('/')[1]
+        repotype = os.getenv("BINDER_PERSISTENT_REQUEST", "").split("/")[1]
     except IndexError:
-        return ''
-    if repotype == 'gh':
-        return 'GitHub'
-    if repotype == 'gl':
-        return 'GitLab'
+        return ""
+    if repotype == "gh":
+        return "GitHub"
+    if repotype == "gl":
+        return "GitLab"
     return repotype.capitalize()
 
 
@@ -63,12 +64,12 @@ class OfflineNotebookConfig(Configurable):
     """
 
     repository_id = Callable(
-        default_value=lambda: os.getenv('BINDER_REPO_URL', ''),
+        default_value=lambda: os.getenv("BINDER_REPO_URL", ""),
         help="""
         A callable that returns the repository ID.
         This is used when storing and retrieving notebooks.
         Default is the value of the `BINDER_REPO_URL` environment variable.
-        """
+        """,
     ).tag(config=True)
 
     repository_label = Callable(
@@ -77,25 +78,27 @@ class OfflineNotebookConfig(Configurable):
         A callable that returns the repository label.
         Default is to parse the `BINDER_PERSISTENT_REQUEST` environment
         variable.
-        """
+        """,
     ).tag(config=True)
 
     repository_ref_url = Callable(
-        default_value=lambda: os.getenv('BINDER_REF_URL', ''),
+        default_value=lambda: os.getenv("BINDER_REF_URL", ""),
         help="""
         A callable that returns the persistent Binder URL.
         Default is the value of the `BINDER_REF_URL` environment variable.
-        """
+        """,
     ).tag(config=True)
 
     binder_persistent_url = Callable(
-        default_value=lambda: (os.getenv('BINDER_LAUNCH_HOST', '') +
-                               os.getenv('BINDER_PERSISTENT_REQUEST', '')),
+        default_value=lambda: (
+            os.getenv("BINDER_LAUNCH_HOST", "")
+            + os.getenv("BINDER_PERSISTENT_REQUEST", "")
+        ),
         help="""
         A callable that returns the repository reference URL.
         Default is the values of the `BINDER_LAUNCH_HOST` and
         `BINDER_PERSISTENT_REQUEST` environment variables.
-        """
+        """,
     ).tag(config=True)
 
 
@@ -103,26 +106,31 @@ def _jupyter_server_extension_paths():
     """
     Jupyter server extension
     """
-    return [{'module': 'jupyter_offlinenotebook'}]
+    return [{"module": "jupyter_offlinenotebook"}]
 
 
 def _jupyter_nbextension_paths():
     """
     Jupyter notebook extension
     """
-    return [dict(
-        section="notebook",
-        src="./static",
-        dest="jupyter-offlinenotebook",
-        require="jupyter-offlinenotebook/main")]
+    return [
+        dict(
+            section="notebook",
+            src="./static",
+            dest="jupyter-offlinenotebook",
+            require="jupyter-offlinenotebook/main",
+        )
+    ]
 
 
 def load_jupyter_server_extension(nbapp):
     """
     Called during notebook start
     """
-    nbapp.web_app.settings['offline_notebook_config'] = OfflineNotebookConfig(
-        parent=nbapp)
+    nbapp.web_app.settings["offline_notebook_config"] = OfflineNotebookConfig(
+        parent=nbapp
+    )
     route_pattern = url_path_join(
-        nbapp.web_app.settings['base_url'], '/offlinenotebook/config')
-    nbapp.web_app.add_handlers('.*', [(route_pattern, OfflineNotebookHandler)])
+        nbapp.web_app.settings["base_url"], "/offlinenotebook/config"
+    )
+    nbapp.web_app.add_handlers(".*", [(route_pattern, OfflineNotebookHandler)])
