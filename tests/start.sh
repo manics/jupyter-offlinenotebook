179c1d7222d73d7c7bff30520e95134574596209
Simon Li <orpheus+devel@gmail.com>
2021-01-16 17:25:11 +0000

    Replace master with main

--- tests/start.sh
+++ tests/start.sh
@@ -2,7 +2,7 @@
 export BINDER_LAUNCH_HOST=http://localhost
 export BINDER_REPO_URL=https://github.com/manics/jupyter-offlinenotebook
 export BINDER_PERSISTENT_REQUEST=v2/gh/repo
-export BINDER_REF_URL=https://github.com/manics/jupyter-offlinenotebook/tree/master
+export BINDER_REF_URL=https://github.com/manics/jupyter-offlinenotebook/tree/main
 
 exec jupyter-lab --debug --no-browser --LabApp.token= --port=18888 "$@"
 
